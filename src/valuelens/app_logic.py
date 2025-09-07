37dbc3c345170142f0935c1128a0d5cb08232273
Piotr Grabysz <pgrabysz@yahoo.com>
2025-09-07 19:17:17 +0200

    refactor: app_logic.py

--- src/valuelens/app_logic.py
+++ src/valuelens/app_logic.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 from src.valuelens.filtering import Paragraph, filter_relevant
-from src.valuelens.scraping.crawler import crawl_site
+from src.valuelens.scraping.crawler import PageResult, crawl_site
 from src.valuelens.scraping.extractors.factory import Extractor
 from src.valuelens.summarizer import Summarizer
 
@@ -15,30 +15,35 @@ class AppConfig:
 @dataclass
 class SummaryResult:
     summary: str | None
-    relevant_paragraphs: list[Paragraph]
+    relevant_paragraphs: list[Paragraph] | None
     all_paragraphs: list[Paragraph] | None
 
 
 def process_url(url: str, config: AppConfig) -> SummaryResult:
     """Core app logic: scrape a URL and summarize it."""
     page_results = crawl_site(root_url=url, extractor=config.extractor)
-    paragraphs = []
-    for page_result in page_results:
-        for paragraph in page_result.paragraphs:
-            paragraphs.append(Paragraph(text=paragraph, source=page_result.url))
+    paragraphs = _extract_results_into_paragraphs(page_results)
 
     if not page_results:
-        return SummaryResult(summary=None, relevant_paragraphs=[], all_paragraphs=None)
+        return SummaryResult(summary=None, relevant_paragraphs=None, all_paragraphs=None)
 
     relevant_results = filter_relevant(paragraphs)
 
     if not relevant_results:
-        return SummaryResult(
-            summary=None, relevant_paragraphs=relevant_results, all_paragraphs=paragraphs
-        )
+        return SummaryResult(summary=None, relevant_paragraphs=None, all_paragraphs=paragraphs)
 
     text_to_summarize = "\n\n".join(p.text for p in paragraphs)
     summary = config.summarizer.summarize(text_to_summarize)
     return SummaryResult(
         summary=summary, relevant_paragraphs=relevant_results, all_paragraphs=paragraphs
     )
+
+
+def _extract_results_into_paragraphs(page_results: list[PageResult]) -> list[Paragraph]:
+    paragraphs = [
+        Paragraph(text=paragraph, source=page_result.url)
+        for page_result in page_results
+        for paragraph in page_result.paragraphs
+    ]
+
+    return paragraphs
